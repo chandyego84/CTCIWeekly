c41c843d9b626a2f9229ce0dd7300efe92b8392f
chandyego <chandlerjuego101@gmail.com>
2021-08-13 22:59:08 -0700

    REALLY BAD CODE FOR REVERSING AN INT, lol

--- LeetCodeTesting/leetCode.py
+++ LeetCodeTesting/leetCode.py
@@ -1,8 +1,6 @@
-'''
-Longest substring without repeeating characters
-'''
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        '''Longest substring without repeeating characters'''
         charDict = {}
         left = 0
         longest = 0
@@ -24,5 +22,40 @@ class Solution:
         
         return longest
 
-longSubStr = Solution()
-print(longSubStr.lengthOfLongestSubstring(("tmmxyzta")))
+    def reverse(self, x: int) -> int:
+        '''Given a signed 32-bit integer x, return x with its digits reversed. 
+        If reversing x causes the value to go outside 
+        the signed 32-bit integer range [-231, 231 - 1], then return 0.'''
+        # assume environment does not allow you to store 64-bit ints
+        reversed = 0 
+        xLength = len(str(x))
+
+        if (x < 0):
+            x += x * -2
+            xLength -= 1
+            while (xLength != 0):
+                reversed += (x % 10) * (10 ** (xLength - 1))
+                x //= 10
+                xLength -= 1  
+            reversed -= reversed * 2
+        else:
+            while (xLength != 0):
+                reversed += (x % 10) * (10 ** (xLength - 1))
+                x //= 10
+                xLength -= 1    
+        
+        if (reversed < 2 ** 31 or reversed > -2 ** 31):
+            return 0
+
+        return reversed
+
+
+    def longestPalindrome(self, s: str) -> str:
+        '''Given string s, return longest palindromic substring in s'''
+        # can be done using Dynamic Programming
+        
+
+
+
+solt = Solution()
+print(solt.lengthOfLongestSubstring(("tmmxyzta")))
